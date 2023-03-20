b7681ba9b20033bfa97d9119b234ed4f2032dbee
jeonga-Son <jjeong0106@gmail.com>
2023-03-20 17:31:50 +0900

    [design] update bestList/index.vue

--- src/pages/products/bestList/index.vue
+++ src/pages/products/bestList/index.vue
@@ -1,30 +1,50 @@
 <template>
-  <section class="product-section-1">
-  <div class="con prod-list best-item">
-        <div>
-            <ul class="row">
-	            <li class="cell" >
-	                <div style="color:gray" class="title">깜깜배송</div>
-	                <div class="price"><span>갈비탕</span></div>
-	                <div class="price"><span>cj</span></div>
-	                <div class="price"><span>1000원</span></div>
-	            </li>
-        </ul>
+  <div class="container mt-5">
+      <div class="page-banner">
+        <div class="row justify-content-center align-items-center h-100">
+          <div class="col-md-6">
+            <nav aria-label="Breadcrumb">
+              <ul class="breadcrumb justify-content-center py-0 bg-transparent">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item active">Best 100</li>
+              </ul>
+            </nav>
+            <h1 class="text-center">Best Products</h1>
+          </div>
         </div>
+      </div>
     </div>
-  </section>   
+
+    <div class="container">
+      
+      	<!--중단 상품리스트 -->
+
+		<!--상품리스트1-->
+		<section class="product-section-1">
+      <div class="con prod-list best-item">
+        <ul class="row">
+	        <c:forEach items='${bestProductList}' var="product">
+	            <li class="cell" >
+	                <a href="/products/detail/${product.p_id}"><div class="img-box"><span><img src="" /></span></div></a>
+	                <div style="color:gray" class="title"><c:out value="${product.delivery_type}배송" /></div>
+	                <div class="price"><span><c:out value="[${product.brand}]"></c:out></span></div>
+	                <div class="price"><span><c:out value="${product.name}" /></span></div>
+	                <div class="price"><span><c:out value="${product.price}원" /></span></div>
+	            </li>
+	         </c:forEach>
+        </ul>
+      </div>
+    </section> 
+    </div>  
+
+<section class="banner-2">
+  <a href="#"><img src="https://product-image.kurly.com/banner/random-band/pc/img/e4d03436-77aa-4875-86bf-f9655f4c1069.jpg" alt=""></a>
+</section>
 </template>
 
 <script>
 export default {
-  setup() {
-    const name = "";
-
-
-    return{
-      name,
-    }
-  }
+  setup() {}
 
 }
 </script>
